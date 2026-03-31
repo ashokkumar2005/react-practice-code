9ca0bf60ef3216261d903101e5cafafb2ad9864f
Ashokkumar <babajiashok8637@gmail.com>
2026-03-31 22:59:36 +0530

    Initial commit

---
+++ samplereact/src/App.jsx
@@ -0,0 +1,9 @@
+import List from "./components/list.jsx"
+
+function App() {
+  return(
+    <List></List>
+  )
+}
+
+export default App
---
+++ samplereact/src/components/car.jsx
@@ -0,0 +1,10 @@
+ const car = (props)=>{
+
+    return(
+        <>
+          <h2>Hi, Im {props.name1} and my age {props.age}</h2>
+        </>
+    )
+ }
+
+ export default car;
